95e8ea0b39da6de8b207710e4ae10350072cbe1e
Victor2297 <victorcatanoi97@gmail.com>
2020-11-01 17:50:29 +0200

    TC cu search este modificat

--- src/test/java/com/inetBanking/testCases/TC_SearchCustomer.java
+++ src/test/java/com/inetBanking/testCases/TC_SearchCustomer.java
@@ -2,6 +2,6 @@ package com.inetBanking.testCases;
 
 public class TC_SearchCustomer {
 	public static void main(String[]args) {
-		System.out.println("Hello");
+		System.out.println("Hello Moldova");
 	}
 }
